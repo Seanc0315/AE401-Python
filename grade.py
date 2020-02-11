e27421bd60f7306bd6e9200fa15e50f877e14a98
Seanc0315 <60865299+Seanc0315@users.noreply.github.com>
2020-02-11 12:04:03 +0800

    顯示及格或不及格

---
+++ grade.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 11 11:07:53 2020
+
+@author: James
+"""
+
+grade = input("請輸入你的成績:")
+if int(grade) >= 60:
+    print("恭喜你，及格了!")
+else:
+    print("抱歉，不及格。")
