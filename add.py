087e9a51fb32cf4800b0d32c2b5b6ae320c031db
Seanc0315 <60865299+Seanc0315@users.noreply.github.com>
2020-02-20 10:40:26 +0800

    自訂義函式

---
+++ add.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 17 11:43:50 2020
+
+@author: James
+"""
+
+def add3(a, b, c):
+    answer = a + b + c
+    return answer
+
+x = add3(57,38,29)
+y = add3(65,20,83)
+z = add3(40,59,68)
+
+print(x,y,z)
